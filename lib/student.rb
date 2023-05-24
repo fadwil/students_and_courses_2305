4f0ad0096de67f362d792891c6bdeb9357d4153c
Wil Fady <wrfady@gmail.com>
2023-05-24 11:18:32 -0400

    Complete iteration one

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
 class Student
- 
+  attr_reader :name, :age, :scores
+  
+  def initialize(student_details)
+    @name = student_details[:name]
+    @age = student_details[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
+
+  def grade
+    average = @scores.sum / scores.size.to_f
+    average
+  end
+
 end
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,26 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  it 'exists and has readable attributes' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    expect(student).to be_a(Student)
+    expect(student.name).to eq("Morgan")
+    expect(student.age).to eq(21)
+    expect(student.scores).to eq([])
+  end
+
+  it 'can log student scores' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.scores).to eq([89, 78])
+  end
+
+  it 'can average student scores' do
+    student = Student.new({name: "Morgan", age: 21}) 
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.scores).to eq([89, 78])
+    expect(student.grade).to eq(83.5)
+  end
 end
