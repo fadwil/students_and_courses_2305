f359438f881babd282e7382c47933b936a2954db
Wil Fady <wrfady@gmail.com>
2023-05-24 11:32:59 -0400

    Complete iteration 2

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,23 @@
 class Course
+  attr_reader :name, :capacity, :students
+
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @is_full = false
+  end
+
+  def full?
+    if @students.length == @capacity
+      @is_full = true
+    else
+      false
+    end
+  end
+
+  def enroll(student)
+    @students << student
+  end
 
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,22 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  it 'exists and has readable attributes' do
+    course = Course.new("Calculus", 2) 
+    expect(course.name).to eq("Calculus")
+    expect(course.capacity).to eq(2)
+    expect(course.students).to eq([])
+  end
+
+  it 'can enroll students and check if course is full' do
+    course = Course.new("Calculus", 2)
+    expect(course.full?).to eq(false)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29}) 
+    course.enroll(student1) 
+    course.enroll(student2)
+    expect(course.students).to eq([student1, student2])
+    expect(course.full?).to eq(true)
+  end
 
 end
